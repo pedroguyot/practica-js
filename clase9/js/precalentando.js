6a4f42af72dd4e2a7369b1d31bae08eb5a03f8cf
pedroguyot <pguyot@udesa.edu.ar>
2024-05-17 10:58:29 -0300

    Arranque ejercicios de la clase 9

---
+++ clase9/js/precalentando.js
@@ -0,0 +1,9 @@
+let mensaje = 'vas a salir de tu casa?'
+
+let resultado = confirm(mensaje)
+
+if(resultado == true) {
+    alert('Chequea el pronostico del tiempo antes de salir')
+} else {
+    alert('Que bueno que te quedes. Va a llover mucho hoy.')
+}
