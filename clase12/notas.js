49e0c6bf6d036e1186c0921dda21cec94efb0654
pedroguyot <pguyot@udesa.edu.ar>
2024-06-07 13:00:39 -0300

    Notas clase 12

---
+++ clase12/notas.js
@@ -0,0 +1,15 @@
+//VALIDANDO EVENTOS
+//focus: 
+
+
+//Para captuar un campo usando clases de css
+let campoEmail = document.querySelector('.email');
+campoEmail.addEventListener('focus', function() {
+    //Nuestro codigo a ejecutar
+})
+
+
+
+//Submit se verifica en el momento del envio del formulario
+let formulario = document.querySelector('form');
+formulario.addEventListener
